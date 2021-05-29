63065dd9be055c6fb6e7b0d1212d520eab633b65
saurav ghosh <josesaurav@gmail.com>
2021-05-29 10:21:32 +0600

    changes to sidebar

--- src/SidebarChannel.js
+++ src/SidebarChannel.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 import "./SidebarChannel.css";
 
-function SidebarChannel({id,channel}) {
+function SidebarChannel({ id, channel }) {
     return (
         <div className="sidebarChannel">
-        // This is the channel name
-        <h4><span className="sidebarChannel__hash">#</span>Youtube</h4>
-            
+            // This is the channel name // This name will show on the sidebar
+            <h4>
+                <span className="sidebarChannel__hash">#</span>Youtube
+            </h4>
         </div>
-    )
+    );
 }
 
-export default SidebarChannel
+export default SidebarChannel;
