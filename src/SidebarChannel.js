d5a5eb9f9fd5a6c6641b04577db53d98b85333ba
Saurav Ghosh <76748365+saurav-ghosh@users.noreply.github.com>
2021-05-29 10:13:54 +0600

    Update SidebarChannel.js

--- src/SidebarChannel.js
+++ src/SidebarChannel.js
@@ -4,6 +4,7 @@ import "./SidebarChannel.css";
 function SidebarChannel({id,channel}) {
     return (
         <div className="sidebarChannel">
+        // This is the channel name
         <h4><span className="sidebarChannel__hash">#</span>Youtube</h4>
             
         </div>
